682749877f1669b83cf949cd818e0e7eb2d2fb44
Moniroul Islam Russell <95379583+moniroul@users.noreply.github.com>
2025-02-23 14:14:48 +0600

    almost done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,25 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <form action="{{ route('home') }}" method="GET">
+        <input type="text" name="search" placeholder="Search by name or email">
+        <button type="submit">Search</button>
+    </form>
+
+    <a href="{{ route('upload') }}" class="btn btn-primary">Upload Image</a>
+
+    <div class="row">
+        @foreach($images as $image)
+        <div class="col-md-4">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+                <img src="{{ asset('storage/' . $image->image_path) }}" class="card-img-top" alt="Uploaded Image">
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <h5 class="card-title">Uploaded by: {{ $image->user->name }} ({{ $image->user->email }})</h5>
+                    <p class="card-text">Button: {{ $image->button_type }}</p>
                 </div>
             </div>
         </div>
+        @endforeach
     </div>
 </div>
 @endsection
